eee47212233be85081052ce831ec50711140b91c
Ujjwal Kaur <75835619+UjjwalKaur@users.noreply.github.com>
2026-05-31 12:40:39 +0530

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.py
@@ -0,0 +1,27 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        # for an array of length size, number of elements in the subset must be 2^size
+        # use backtracking using recursive dfs
+        res = []
+        subset = []
+
+        def dfs(i):
+            # base case
+            if (i >= len(nums)):
+                res.append(subset.copy())
+                return 
+
+            # decision : include nums[i]
+            subset.append(nums[i])
+            dfs(i + 1)
+
+            # decision : do not include nums[i]
+            subset.remove(nums[i])
+            dfs(i + 1)
+
+        dfs(0)
+        return res
+
+
+
+        
